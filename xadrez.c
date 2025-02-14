c80ff4b88052cddb4e174857ae140ad6e68589f8
Marcelo Ferreira <35692178+marcelo-fervi@users.noreply.github.com>
2025-02-14 17:51:13 +0000

    Progresso no Desafio Novato

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,43 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    const int bispoMovimentacaoCasas = 5; //5 casas na diagonal superior direita
+    const int torreMovimentacaoCasas = 5; //5 casas para a direita
+    const int rainhaMovimentacaoCasas = 8; //8 casas para a esquerda
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int quantiaDeCasasRestantesParaOBispoAndar = bispoMovimentacaoCasas;
+    
+    printf("Movimentação do Bispo:\n");
+    while (quantiaDeCasasRestantesParaOBispoAndar > 0) {
+        quantiaDeCasasRestantesParaOBispoAndar--;
+
+        printf("Direita, Cima\n");
+    }
+    printf("\n");
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    int quantiaDeCasasRestantesParaATorreAndar = torreMovimentacaoCasas;
+
+    printf("Movimentação da Torre:\n");
+    do {
+        quantiaDeCasasRestantesParaATorreAndar--;
+
+        printf("Direita\n");
+    }
+    while (quantiaDeCasasRestantesParaATorreAndar > 0);
+    printf("\n");
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    int quantiaDeCasasRestantesParaARainhaAndar = rainhaMovimentacaoCasas;
+
+    printf("Movimentação da Rainha:\n");
+    for (int indiceDoMovimento = 0; indiceDoMovimento < quantiaDeCasasRestantesParaARainhaAndar; indiceDoMovimento++) {
+        printf("Esquerda\n");
+    }
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
