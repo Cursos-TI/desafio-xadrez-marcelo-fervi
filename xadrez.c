b57d501c85e481fe4fdb03ad71c191ea7a88456f
Marcelo Ferreira <35692178+marcelo-fervi@users.noreply.github.com>
2025-02-14 19:57:15 +0000

    Progresso no Desafio Mestre

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,64 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+//Anda para um conjunto de direções definidas por flags:
+// 0x01 - Cima;
+// 0x02 - Baixo;
+// 0x04 - Esquerda;
+// 0x08 - Direita
+void andarCasa(int direcoes, int quantiaDeCasasRestantes) {
+    bool paraCima = (direcoes & 0x01) == 0x01;
+    bool paraBaixo = (direcoes & 0x02) == 0x02;
+    bool paraEsquerda = (direcoes & 0x04) == 0x04;
+    bool paraDireita = (direcoes & 0x08) == 0x08;
+
+    //Caso exista 2 movimentos contrários de mesma direção, anule-os e atualize o estado dos booleanos.
+    if (paraCima && paraBaixo)
+        direcoes -= 0x01 | 0x02;
+
+    if (paraEsquerda && paraDireita)
+        direcoes -= 0x04 | 0x08;
+
+    paraCima = (direcoes & 0x01) == 0x01;
+    paraBaixo = (direcoes & 0x02) == 0x02;
+    paraEsquerda = (direcoes & 0x04) == 0x04;
+    paraDireita = (direcoes & 0x08) == 0x08;
+
+    if (paraEsquerda) {
+        if (paraCima)
+            printf("Esquerda, Cima\n");
+        else if (paraBaixo)
+            printf("Esquerda, Baixo\n");
+        else
+            printf("Esquerda\n");
+    }
+    else if (paraDireita) {
+        if (paraCima)
+            printf("Direita, Cima\n");
+        else if (paraBaixo)
+            printf("Direita, Baixo\n");
+        else
+            printf("Direita\n");
+    }
+    else if (paraCima) {
+        printf("Cima\n");
+    }
+    else if (paraBaixo) {
+        printf("Baixo\n");
+    }
+    else {
+        //Não está andando para nenhuma direção
+        return;
+    }
+
+    quantiaDeCasasRestantes--;
+
+    if (quantiaDeCasasRestantes > 0) {
+        andarCasa(direcoes, quantiaDeCasasRestantes);
+    }
+}
+
+void desafioNovatoEAventureiro() {
     // Nível Novato - Movimentação das Peças
     const int bispoMovimentacaoCasas = 5; //5 casas na diagonal superior direita
     const int torreMovimentacaoCasas = 5; //5 casas para a direita
@@ -12,7 +69,7 @@ int main() {
 
     // Implementação de Movimentação do Bispo (5 casas na diagonal, direita e cima)
     int quantiaDeCasasRestantesParaOBispoAndar = bispoMovimentacaoCasas;
-    
+
     printf("Movimentação do Bispo:\n");
     while (quantiaDeCasasRestantesParaOBispoAndar > 0) {
         quantiaDeCasasRestantesParaOBispoAndar--;
@@ -29,8 +86,7 @@ int main() {
         quantiaDeCasasRestantesParaATorreAndar--;
 
         printf("Direita\n");
-    }
-    while (quantiaDeCasasRestantesParaATorreAndar > 0);
+    } while (quantiaDeCasasRestantesParaATorreAndar > 0);
     printf("\n");
 
     // Implementação de Movimentação da Rainha (8 casas para a esquerda)
@@ -62,13 +118,78 @@ int main() {
         }
     }
     printf("\n");
+}
 
+void desafioMestre() {
     // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    const int bispoMovimentacaoCasas = 5; //5 casas na diagonal superior direita
+    const int torreMovimentacaoCasas = 5; //5 casas para a direita
+    const int rainhaMovimentacaoCasas = 8; //8 casas para a esquerda
+
+    // Implementação de Movimentação do Bispo (5 casas na diagonal, direita e cima)
+    int quantiaDeCasasRestantesParaOBispoAndar = bispoMovimentacaoCasas;
+
+    printf("Movimentação do Bispo:\n");
+    int totalDeCasasAndadasDoBispo = 0;
+    while (totalDeCasasAndadasDoBispo < bispoMovimentacaoCasas) {
+        for (int casaAtual = 0; casaAtual < bispoMovimentacaoCasas; casaAtual++) {
+            andarCasa(1 | 8, 1);
+
+            totalDeCasasAndadasDoBispo++;
+        }
+    }
+    printf("\n");
+
+    // Implementação de Movimentação da Torre (5 casas para a direita)
+    int quantiaDeCasasRestantesParaATorreAndar = torreMovimentacaoCasas;
+
+    printf("Movimentação da Torre:\n");
+    andarCasa(8, quantiaDeCasasRestantesParaATorreAndar);
+    printf("\n");
+
+    // Implementação de Movimentação da Rainha (8 casas para a esquerda)
+    int quantiaDeCasasRestantesParaARainhaAndar = rainhaMovimentacaoCasas;
+
+    printf("Movimentação da Rainha:\n");
+    andarCasa(4, quantiaDeCasasRestantesParaARainhaAndar);
+    printf("\n");
+
+    const int quantiaDeCasasPorEstagioParaOCavaloAndar = 1; //1 casa para cima e direita
+    const int totalDeEstagiosDeDirecaoDoCavalo = 2;
+    int casasRestantesDoEstagioAtualDoCavalo = quantiaDeCasasPorEstagioParaOCavaloAndar;
+    int totalDeCasasAndadasDoCavalo = 0;
+
+    printf("Movimentação do Cavalo:\n");
+    while (true) {
+        bool concluiuOsEstagios = false;
+        for (int estagioAtual = 0; estagioAtual < totalDeEstagiosDeDirecaoDoCavalo; estagioAtual++) {
+            for (int casaAtual = 0; casaAtual < quantiaDeCasasPorEstagioParaOCavaloAndar; casaAtual++) {
+                if (estagioAtual == 0) {
+                    //Estágio 1, somente para cima
+                    andarCasa(1, 1);
+                }
+                else {
+                    //Estágio 2, somente para a direita
+                    andarCasa(8, 1);
+                }
+
+                totalDeCasasAndadasDoCavalo++;
+            }
+        }
+
+        if (totalDeCasasAndadasDoCavalo >= quantiaDeCasasPorEstagioParaOCavaloAndar * totalDeEstagiosDeDirecaoDoCavalo) {
+            break;
+        }
+    }
+    printf("\n");
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+}
+
+int main() {
+    //desafioNovatoEAventureiro();
+    desafioMestre();
 
     return 0;
 }
